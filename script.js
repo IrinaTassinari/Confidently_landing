f3a2124af86c7ca5a660ad671d2f214384db9b53
IrinaTassinari <irinatassinari@gmail.com>
2026-05-16 17:25:48 +0200

    added readme

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+if ("scrollRestoration" in history) {
+  history.scrollRestoration = "manual";
+}
+
+window.addEventListener("load", () => {
+  window.scrollTo(0, 0);
+});
+
 const headerLang = document.querySelector(".header-lang");
 const headerLangButton = document.querySelector(".header-lang-button");
 
